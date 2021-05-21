4d4dfe9a4a79571ada2a888a46c77a4b1acbae61
Erjankydyrov <erjankydyrov29@gmail.com>
2021-05-21 13:48:34 +0600

    corected

--- src/axios.js
+++ src/axios.js
@@ -3,4 +3,4 @@ import axios from "axios";
 const instance = axios.create();
 instance.defaults.baseURL = "https://builder-3fa6d-default-rtdb.firebaseio.com";
 
-export default instance;
+export default instance; 
