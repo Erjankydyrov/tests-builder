e3043eef7752bbfd04ec269e7e368d45026ad0c7
Erjankydyrov <erjankydyrov29@gmail.com>
2021-05-24 00:37:30 +0600

    corected one ditale in order

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -1,10 +1,31 @@
 import classes from "./Order.module.css";
 
 const Order = ({ name, phone, address, ingredients, price }) => {
+  const labels = {
+    PBuns: "Pretzel",
+    Bread: "Cake",
+    BBuns: "Waffle with ice cream",
+    Crois: "Croissant",
+    Ecler: "Jelly cookies",
+    MBuns: "Waffle",
+    PBunsChocolate: "Chocolate pretzel",
+    BreadChocolate: "Chocolate cake",
+    BBunsChocolate: "Waffle with сhocolate ice cream",
+    CroisChocolate: "Vanilla croissant",
+    EclerChocolate: "Jelly cookies with сhocolate",
+    MBunsChocolate: "Chocolate waffle",
+    PBunsVanilla: "Vanilla pretzel",
+    BreadVanilla: "Vanilla cake",
+    BBunsVanilla: "Waffle with vanilla ice cream",
+    CroisVanilla: "Chocolate croissant",
+    EclerVanilla: "Jelly cookies with vanilla",
+    MBunsVanilla: "Vanilla waffle",
+  };
+
   const outputIngredients = Object.keys(ingredients).map((ingredient) => (
-    <em>
-      {ingredient} - {ingredients[ingredient]}
-    </em>
+    <li>
+      {labels[ingredient]} - {ingredients[ingredient]}
+    </li>
   ));
 
   return (
@@ -12,7 +33,7 @@ const Order = ({ name, phone, address, ingredients, price }) => {
       <div>
         {name}, {phone}, {address}
       </div>
-      <div>{outputIngredients}</div>
+      <ul>{outputIngredients}</ul>
       <strong>{price}</strong>
     </div>
   );
